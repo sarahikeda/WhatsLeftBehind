a08a15f1a834ee2b886755eb7dd9b953865c4075
sarahikeda <sarahikeda@gmail.com>
2017-07-26 21:01:24 -0500

    Remove commas from tokens and rename function

--- tweets.js
+++ tweets.js
@@ -2,17 +2,20 @@ var sentiment = require('sentiment');
 var https = require('https');
 var cheerio = require('cheerio');
 
-var options = {
-  host: 'twitter.com',
-  path: '/search?q=near%3A%22Manchester%2C+England%22+within%3A15mi+since%3A2015-07-02+until%3A2015-07-19&ref_src=twsrc%5Etfw&ref_url=https%3A%2F%2Ftwitter.com%2Fsettings%2Fwidgets%2Fnew%2Fsearch'
-};
+function getTweets(){
+  var options = {
+    host: 'twitter.com',
+    path: '/search?q=near%3A%22Manchester%2C+England%22+within%3A15mi+since%3A2015-07-02+until%3A2015-07-19&ref_src=twsrc%5Etfw&ref_url=https%3A%2F%2Ftwitter.com%2Fsettings%2Fwidgets%2Fnew%2Fsearch'
+  };
 
-var req = https.request(options, function(res) {
-  res.setEncoding('utf8');
-  res.on('data', function (content) {
-  scrapePage(content)
+  var req = https.request(options, function(res) {
+    res.setEncoding('utf8');
+    res.on('data', function (content) {
+    scrapePage(content)
+    });
   });
-});
+  req.end();
+}
 
 function scrapePage(content){
   var $ = cheerio.load(content);
@@ -25,11 +28,12 @@ function scrapePage(content){
 function sentimentAnalyze(tweet){
   var tweetResults = sentiment(tweet)
   if ((tweetResults['comparative']) < 0) {
-      console.dir('Negative: ' + tweetResults.tokens)
+      console.dir('Negative: ' + tweetResults.tokens.join(" "))
   } else if ((tweetResults['comparative']) > 0) {
-      console.dir('Positive: ' + tweetResults.tokens)
+      console.dir('Positive: ' + tweetResults.tokens.join(" "))
   } else {
-      console.dir('Neutral: ' + tweetResults.tokens)
+      console.dir('Neutral: ' + tweetResults.tokens.join(" "))
   }
 }
-req.end();
+
+getTweets();
